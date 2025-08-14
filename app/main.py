9af980cae360d556db826d708beb4df654aed5fb
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2025-08-14 19:31:25 +0530

    added main.py

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from .db import engine, Base
+from . import websocket
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        yield
+        # Shutdown
+        # (Add cleanup logic here if needed)
+
+app = FastAPI(title="Live Chat API", lifespan=lifespan)
+
+app.include_router(websocket.router)
+
