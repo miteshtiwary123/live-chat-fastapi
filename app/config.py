4567c88b1eafea943f8a1322f80f6eefd7bb3308
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2025-08-14 15:47:09 +0530

    added requirements file

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:monu1234@localhost:5432/live_chat"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
