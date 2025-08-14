03e6888a8397f52de2e8cdd950b92b2aec38f8c9
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2025-08-14 18:22:02 +0530

    created models.py

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from .db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(Text, nullable=False)
+    messages = relationship("Message", back_populates="sender")
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(Text, nullable=False)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    sender = relationship("User", back_populates="messages")
